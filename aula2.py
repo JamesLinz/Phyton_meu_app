30163569a14942ea9e59e4544a9d0183a33c4cb1
JamesLinz <jlalencar@gmail.com>
2021-10-07 17:35:28 -0300

    Melhoria na calculadora

--- aula2.py
+++ aula2.py
@@ -1,16 +1,29 @@
-a = 10
-b = 5
+a = int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+print(type(a))
+
 soma = a + b
 subtracao = a - b
 multiplicacao = a * b
 divisao = a / b
 resto = a % b
-print('soma: ' + str(soma))
-print('subtracao: ' + str(subtracao))
-print(multiplicacao)
-print(int(divisao))
-print(divisao)
-print(resto)
+
+print('Soma: {soma}. \nSubtração: {subtracao}. '
+      '\nMultiplicação: {multiplicacao}'
+      '\nDivisão: {divisao}'
+      '\nResto: {resto}'.format(soma=soma,
+                                subtracao=subtracao,
+                                multiplicacao=multiplicacao,
+                                divisao=divisao,
+                                resto=resto))
+
+# print('soma: ' + str(soma))
+# print('subtracao: ' + str(subtracao))
+# print('multiplicacao: ' + str(multiplicacao))
+# print('int(divisao): ' + str(int(divisao)))
+# print('divisao: ' + str(divisao))
+# print('resto: ' + str(resto))
+
 # x = '1'
 # soma2 = int(x) + 1
 # print(soma2)
