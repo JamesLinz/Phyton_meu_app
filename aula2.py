8ac5a309a0a19cd2dd2a57e912ba2c7ad8a44ceb
JamesLinz <jlalencar@gmail.com>
2021-10-07 16:45:22 -0300

    Projeto Criado

---
+++ aula2.py
@@ -0,0 +1,13 @@
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('soma: ' + str(soma))
+print(subtracao)
+print(multiplicacao)
+print(int(divisao))
+print(divisao)
+print(resto)
